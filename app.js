ab35b35f53dc48022907ab8daaab7b4ee55ef806
sanshishen <sanshishen@qq.com>
2016-05-17 15:37:34 +0800

    添加nodejs启动程序入口

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-{
-  "directory" : "public/assets/lib"
-}
+var express = require('express'),
+    port = process.env.PORT || 3000;
+var app = express();
+
+// set static files' path
+app.use(express.static(__dirname + '/public'));
+
+// 监听端口
+app.listen(port);
+console.log('app sanshishen is started on port ' + port);
